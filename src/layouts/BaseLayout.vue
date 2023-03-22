6a3d98f2bed830c95652482294564f5e23e97873
Antonio Del Aguila <56671752+JorgeAntonio@users.noreply.github.com>
2023-03-22 12:30:02 -0500

    base layout

---
+++ src/layouts/BaseLayout.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="bg-gray-100 h-full px-20">
+        <Navbar /> 
+        <main>
+            <router-view />
+        </main>
+    </div>
+</template>
+
+<script setup>
+
+    import Navbar from "../components/Navbar.vue"
+
+</script>
